b6c8dde79388c1adf5c5307c35b6af31a67be4c8
Aitzaz-web <ams845@cornell.edu>
2024-07-16 22:44:37 +0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Aitzaz Munir on 16/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
